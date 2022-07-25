c68bcb37e3018f0f3b17c9f55aef86f8039bcaec
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-07-25 07:39:54 +0300

    done

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -575,8 +575,8 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((e, i) => e[i], arr);
 }
 
 
@@ -598,8 +598,11 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const s = Math.floor(arr.length / 2);
+  const head = arr.slice(0, s);
+  const tail = arr.slice(s, arr.length);
+  return head.concat(tail);
 }
 
 
